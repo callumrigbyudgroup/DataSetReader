26b3bc7778e8a6a837e408ae412d376d56a62eff
Callum Rigby <callum.rigby@udgroup.co.uk>
2017-08-15 14:07:44 +0100

    Add DataSetReader class with test.

---
+++ src/DataSetReader/DataSetReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSetReader
+{
+    public class DataSetReader
+    {
+        public DataSetReader(Stream fileStream)
+        {
+            if (fileStream == null)
+                throw new ArgumentNullException("fileStream");
+        }
+    }
+}
---
+++ test/DataSetReader.Tests/DataSetReaderTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace DataSetReader.Tests
+{
+    [TestFixture]
+    public class DataSetReaderTests
+    {
+        [TestCase]
+        public void WhenStreamIsNull_ThrowsArgumentNullException()
+        {
+            Stream testStream = null;
+
+            Assert.Throws<ArgumentNullException>(() => new DataSetReader(testStream));
+        }
+    }
+}
