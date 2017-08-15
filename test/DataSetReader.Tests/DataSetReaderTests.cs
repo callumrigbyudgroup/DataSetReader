3bad67af249adde633f977177983f853ef2c608b
Callum Rigby <callum.rigby@udgroup.co.uk>
2017-08-15 16:25:35 +0100

    Refactored test logic for asserting returned DataSet isn't empty.

--- test/DataSetReader.Tests/DataSetReaderTests.cs
+++ test/DataSetReader.Tests/DataSetReaderTests.cs
@@ -21,8 +21,13 @@ namespace DataSetReader.Tests
         [TestCase]
         public void WhenStreamIsAnOpenXmlExcelFileAndNotEmpty_AsDataSet_ReturnsDataFromStreamAsADataSet()
         {
+            AssertDataSetIsNotEmpty(Resources.OpenXmlExcelKeypad);
+        }
+
+        private void AssertDataSetIsNotEmpty(byte[] data)
+        {
             DataSet result = null;
-            using (var testStream = new MemoryStream(Resources.OpenXmlExcelKeypad))
+            using (var testStream = new MemoryStream(data))
             {
                 var reader = new DataSetReader(testStream);
                 result = reader.AsDataSet();
@@ -35,29 +40,13 @@ namespace DataSetReader.Tests
         [TestCase]
         public void WhenStreamIsABinaryExcelFileAndNotEmpty_AsDataSet_ReturnsDataFromStreamAsADataSet()
         {
-            DataSet result = null;
-            using (var testStream = new MemoryStream(Resources.BinaryExcelKeypad))
-            {
-                var reader = new DataSetReader(testStream);
-                result = reader.AsDataSet();
-            }
-
-            Assert.IsNotNull(result);
-            Assert.Greater(result.Tables.Count, 0);
+            AssertDataSetIsNotEmpty(Resources.BinaryExcelKeypad);
         }
 
         [TestCase]
         public void WhenStreamIsACsvFileAndNotEmpty_AsDataSet_ReturnsDataFromStreamAsADataSet()
         {
-            DataSet result = null;
-            using (var testStream = new MemoryStream(Encoding.UTF8.GetBytes(Resources.CsvKeypad)))
-            {
-                var reader = new DataSetReader(testStream);
-                result = reader.AsDataSet();
-            }
-
-            Assert.IsNotNull(result);
-            Assert.Greater(result.Tables.Count, 0);
+            AssertDataSetIsNotEmpty(Encoding.UTF8.GetBytes(Resources.CsvKeypad));
         }
     }
 }
