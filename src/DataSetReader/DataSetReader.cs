ab86648380a932247f464273239945c87034bd66
Callum Rigby <callum.rigby@udgroup.co.uk>
2017-08-16 15:08:07 +0100

    CSV working without headers.

--- src/DataSetReader/DataSetReader.cs
+++ src/DataSetReader/DataSetReader.cs
@@ -18,24 +18,68 @@ namespace DataSetReader
 
         public DataSet AsDataSet()
         {
+            var dataSet = new DataSet();
+
             try
             {
                 using (IExcelDataReader excelReader = ExcelReaderFactory.CreateReader(this.fileStream))
-                {
-                    return excelReader.AsDataSet();
-                }
+                    dataSet = excelReader.AsDataSet();
             }
             catch (NotSupportedException)
             {
-                using (var csvReader = new CsvReader(new StreamReader(this.fileStream), true))
+                using (var csvReader = new CsvReader(new StreamReader(this.fileStream), false))
                 {
-                    var ds = new DataSet();
                     var dt = new DataTable();
-                    dt.Load(csvReader);
-                    ds.Tables.Add(dt);
-                    return ds;
+
+                    int fieldCount = csvReader.FieldCount;
+                    dt = AddColumns(dt, fieldCount);
+                    dt = LoadData(dt, csvReader);
+
+                    dataSet.Tables.Add(dt);
                 }
-            }            
+            }
+
+            return dataSet;
+        }
+
+        private DataTable AddColumns(DataTable table, int count)
+        {
+            for (int i = 0; i < count; i++)
+                table.Columns.Add($"Column{i}", typeof(string));
+
+            return table;
+        }
+
+        private DataTable LoadData(DataTable table, CsvReader dataReader)
+        {
+            while (dataReader.ReadNextRecord())
+                table = AddRow(table, dataReader);
+
+            return table;
+        }
+
+        private DataTable AddRow(DataTable table, CsvReader dataReader)
+        {
+            DataRow newRow = CreateRowWithData(table, dataReader);
+            table.Rows.Add(newRow);
+
+            return table;
+        }
+
+        private DataRow CreateRowWithData(DataTable table, CsvReader dataReader)
+        {
+            DataRow newRow = table.NewRow();
+            newRow = AddDataToRow(newRow, dataReader, table.Columns.Count);
+
+            return newRow;
+        }
+
+        private DataRow AddDataToRow(DataRow row, CsvReader dataReader, int fieldCount)
+        {
+            for (int i = 0; i < fieldCount; i++)
+                row[i] = dataReader[i];
+
+            return row;
         }
     }
 }
